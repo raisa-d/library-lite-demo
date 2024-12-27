718cae906a0c01735080b281d43b8be3d0671c42
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-12-27 13:02:41 -0500

    imported express, started writing get request

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+// import express with ES Modules
+import express from 'express';
+
+// create instance of Express app
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('If you can see this, the server sent a response!')
+})
+
+// have server listen on PORT
+app.listen(8000, () => console.log('Server is running away!!!'))
