01f060331ce0f69ab029636233a350f5ef80ad79
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-12-27 21:50:19 -0500

    wrote script for package.json, wrote another 1.5 pages of article to accompany project

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from 'express';
 // create instance of Express app
 const app = express();
 
+// handle GET request
 app.get('/', (request, response) => {
     response.send('If you can see this, the server sent a response!')
 })
