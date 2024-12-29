36f5ebbc4cb48da52c73bf64eac7df136e214c8a
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-12-29 17:19:44 -0500

    add form to html, get request body in server

--- server.js
+++ server.js
@@ -3,13 +3,25 @@ import express from 'express'; // import express with ES Modules
 const app = express(); // create instance of Express app
 const dirname = import.meta.dirname; // get dirname
 
-// MIDDLEWARE - next step in article
-// app.use(express.static('public'));
+/* ===========
+MIDDLEWARE
+=========== */
+app.use(express.urlencoded({ extended: true }))
 
+
+/* ===========
+ROUTE HANDLERS 
+=========== */
 // handle GET request
 app.get('/', (request, response) => {
     response.sendFile(`${dirname}/index.html`);
-})
+});
+
+// handle POST request
+app.post('/add', (request, response) => {
+    console.log(request);
+    response.redirect('/');
+});
 
 // have server listen on PORT
-app.listen(8000, () => console.log('Server is running away!!!'))
+app.listen(8000, () => console.log('Server is running away!!!'));
