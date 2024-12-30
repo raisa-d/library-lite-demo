332a7ef7d36530938675bb9e7832605fb0b8db4e
Raisa Dorzback <raisa.dorzback@gmail.com>
2024-12-29 22:22:40 -0500

    connected to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
 // server.js
+/* ===========
+IMPORTS
+=========== */
 import express from 'express'; // import express with ES Modules
+import { MongoClient, ServerApiVersion } from 'mongodb'; // import MongoClient with ES Modules
+
+/* ===========
+VARIABLES
+=========== */
 const app = express(); // create instance of Express app
 const dirname = import.meta.dirname; // get dirname
+const PORT = process.env.PORT;
+const password = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://j97129688:${password}@cluster0.u0qtw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+/* ===========
+CONNECTING TO DATABASE
+=========== */
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+async function run() {
+    try {
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+      await client.close();
+}
+}
+run().catch(console.dir);
 
 /* ===========
 MIDDLEWARE
@@ -24,4 +59,4 @@ app.post('/add', (request, response) => {
 });
 
 // have server listen on PORT
-app.listen(8000, () => console.log('Server is running away!!!'));
+app.listen(PORT, () => console.log('Server is running away!!!'));
