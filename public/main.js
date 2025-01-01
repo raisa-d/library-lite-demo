a0dea769bfb712663dac59732303f7464500117b
Raisa Dorzback <raisa.dorzback@gmail.com>
2025-01-01 18:22:40 -0500

    add delete function

---
+++ public/main.js
@@ -0,0 +1,9 @@
+// const deleteButton = document.querySelectorAll('.delete');
+
+// Array.from(deleteButton).forEach(element => {
+//     element.addEventListener(deleteBook);
+// });
+
+// function deleteBook() {
+//     console.log('deleting book')
+// }
--- server.js
+++ server.js
@@ -34,30 +34,54 @@ async function connectToDatabase() {
 SERVER AND API
 =========== */
 function createServer(books) {
+  app.set('view engine', 'ejs');
+
   /* ===========
   MIDDLEWARE
   =========== */
-  app.use(express.urlencoded({ extended: true }))
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.static('public'));
 
   /* ===========
   ROUTE HANDLERS 
   =========== */
   // handle GET request
   app.get('/', async (request, response) => {
-    const cursor = await books.find().toArray()
-    console.log(cursor);
-    response.sendFile(`${dirname}/index.html`);
+    try {
+      const cursor = await books.find();
+      const bookList = await cursor.toArray();
+      response.render('index.ejs', { bookList: bookList }) // render EJS, pass in bookList
+    } catch(err) {
+      console.error(`Error retrieving list of books ${err}`);
+    };
   });
 
   // handle POST request - add new book
   app.post('/add', async (request, response) => {
     try {
-      const result = await books.insertOne(request.body);
+      const result = await books.insertOne({
+        title: request.body.title,
+        author: request.body.author,
+        read: false
+      });
       console.log(result);
       response.redirect('/');
     } catch(err) {
       console.error(`Could not add book: ${err}`);
       response.status(500);
+    };
+  });
+
+  // handle DELETE request - delete book
+  app.delete('/delete', async (request, response) => {
+    try {
+      // delete book by title
+      const result = await books.deleteOne({title: request.body.title});
+      console.log('Book deleted');
+      response.json('Book deleted');
+    } catch(err) {
+      console.error(`Failed to delete book: ${err}`);
     };
   });
 
